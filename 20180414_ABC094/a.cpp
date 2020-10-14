97d9ed2be5005be40ab582b1ef1a7cc7f049c4ec
MiYA LAB <60140464+miyalab@users.noreply.github.com>
2020-10-14 23:09:23 +0900

    Add files via upload

---
+++ 20180414_ABC094/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b,x;
+    cin >> a >> b >> x;
+
+    if(x-a<=b && x-a>=0) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
