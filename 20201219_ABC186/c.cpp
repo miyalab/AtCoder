111faa878d559e9ddc0b0777f1bcc988ae814e56
MiYA LAB <60140464+miyalab@users.noreply.github.com>
2020-12-20 14:28:32 +0900

    2020.12.20 abc186

--- 20201219_ABC186/c.cpp
+++ 20201219_ABC186/c.cpp
@@ -1,11 +1,33 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
     int n;
     cin >> n;
-
     
+    int ans = n;
+    string check;
+    stringstream ss;
+
+    for(int i=1; i<=n; i++){
+        // 10進数チェック
+        check = to_string(i);
+        if(check.find('7')!=string::npos){
+            ans--;
+            continue;
+        }
+
+        //　8進数チェック
+        ss.str("");
+        ss.clear(std::stringstream::goodbit);
+        ss << oct << i;
+        check = ss.str();
+        if(check.find('7')!=string::npos){
+            ans--;
+            continue;
+        }
+    }
+
+    cout << ans << endl;
 }
--- 20201219_ABC186/d.cpp
+++ 20201219_ABC186/d.cpp
@@ -4,5 +4,27 @@ using namespace std;
 
 int main()
 {
-    
+    long long i, j;
+    long long n;
+
+    cin >> n;
+
+    // データ入力
+    vector<long long> a(n);
+    for(i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    // 演算
+    long long sum = 0;
+    long long ans = 0;
+    for(i=0; i<n-1; i++){
+        sum += a[i];
+        ans += a[i+1]*(i+1) - sum;
+
+    }
+
+    cout << ans << endl;
 }
